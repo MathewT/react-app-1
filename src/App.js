447a100353715e36a4f53a0ce255e3799f86a784
Sunil Thomas <mathew.thomas@longdivision.com>
2020-08-07 11:51:19 -0700

    hello world
    Signed-off-by: Sunil Thomas <mathew.thomas@longdivision.com>

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Person from './Person/Person.js'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello, world</h1>
+      <Person/>
+    </div>
+  );
+}
+
+export default App;
