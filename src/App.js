dd090eef30f63b9e8191feca5bf35b22b34543d8
Sunil Thomas <mathew.thomas@longdivision.com>
2020-08-08 05:53:08 -0700

    App extends Component; switchNameHandler
    Signed-off-by: Sunil Thomas <mathew.thomas@longdivision.com>

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person.js'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>hello, world</h1>
-      <Person name="Adam" age="15">My hobby is computer programming.</Person>
-      <Person name="Chuck" age="54"/>
-      <Person name="Stephen" age="56"/>
-    </div>
-  );
+class App extends Component{
+  state = {
+    persons: [
+      {name: "Amy", age: 28},
+      {name: "Bill", age: 46},
+      {name: "Joe", age: 56}
+    ],
+    otherState: "some other state data"
+  };
+  
+  switchNameHandler = () => {
+    this.setState( {
+      persons: [
+        {name: "Joyce", age: 64},
+        {name: "Jed", age: 67},
+        {name: "Madhavi", age: 45}
+      ]
+    });
+    console.log(this.state.persons);
+  }; 
+
+  render() {
+    return (
+      <div className="App">
+        <h1>hello, world</h1>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My hobby is computer programming.</Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+      </div>
+    );
+  }
 }
 
 export default App;
