5c9e32b0f91f0aeb9bace561f810dc2382b17bf2
Sunil Thomas <mathew.thomas@longdivision.com>
2020-08-09 13:11:00 -0700

    two way binding, switchedNameCount display
    Signed-off-by: Sunil Thomas <mathew.thomas@longdivision.com>

--- src/App.js
+++ src/App.js
@@ -9,33 +9,57 @@ class App extends Component{
       {name: "Bill", age: 46},
       {name: "Joe", age: 56}
     ],
-    otherState: "some other state data"
+    otherState: "some other state data",
+    switchedNameCount: 0
   };
   
+  pausecomp = (millis) => 
+  {
+      var date = new Date();
+      var curDate = null;
+      do { curDate = new Date(); }
+      while(curDate-date < millis);
+  }
   switchNameHandler = () => {
+    let cnt = this.state.switchedNameCount;
+    cnt++;
     this.setState( {
       persons: [
         {name: "Terri", age: 64},
         {name: "Anne", age: 67},
         {name: "Mike", age: 45}
-      ]
+      ],
+      switchedNameCount: cnt
     });
-    console.log(this.state);
+    // console.log(this.state);
   }; 
   
   sayHello = (name) => {
     console.log(`${name}, saying hello`);
+    console.log(this.state);
+  };
+  
+  nameChangedHandler = (event) => {
+    console.log(event.target.value);
+    this.setState( {
+      persons: [
+        {name: event.target.value, age: 64},
+        {name: "Anne", age: 67},
+        {name: "Mike", age: 45}
+      ]
+    });
   };
 
   render() {
     return (
       <div className="App">
-        <h1>hello, world</h1>
+        <h1>hello, world {this.state.switchedNameCount}</h1>
         <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person 
           name={this.state.persons[0].name} 
           age={this.state.persons[0].age}
-          click={this.sayHello.bind(this, this.state.persons[0].name)}>My hobby is computer programming.
+          click={this.sayHello.bind(this, this.state.persons[0].name)}
+          changeHandler={this.nameChangedHandler}>My hobby is computer programming.
         </Person>
         <Person 
           name={this.state.persons[1].name} 
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -8,6 +8,7 @@ const person = (props) => {
     <div>
       <p>I'm {props.name} component! I am {props.age} years old.</p>
       {/* <p>{props.children}</p> */}
+      <input type="text" onChange={props.changeHandler} value={props.name} />
       <button onClick={props.click}>Say Hello</button>
     </div>
     );
